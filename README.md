7f3f056ac922c6931c2960b551c19e6ebca0d002
yimeng <1982337+yimeng@users.noreply.github.com>
2020-01-07 15:33:19 +0800

    add docker

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # kubeadm
+kubeadm init  --pod-network-cidr=10.244.0.0/16   --image-repository registry.aliyuncs.com/google_containers
+kubeadm config images list
+
--- docker.sh
+++ docker.sh
@@ -5,3 +5,12 @@ sudo yum install -y yum-utils device-mapper-persistent-data lvm2
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 
 sudo yum install docker-ce docker-ce-cli containerd.io -y
+
+sudo mkdir -p /etc/docker
+sudo tee /etc/docker/daemon.json <<-'EOF'
+{
+  "registry-mirrors": ["https://bi4c72kn.mirror.aliyuncs.com"]
+}
+EOF
+sudo systemctl daemon-reload
+sudo systemctl restart docker
